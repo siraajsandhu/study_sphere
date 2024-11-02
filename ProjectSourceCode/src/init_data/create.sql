1c25a6489e3a353e6b5e988cd1f41e5808514ed2
Vibha Joshi <vijo7860@colorado.edu>
2024-11-02 13:17:24 -0600

    created tables

---
+++ ProjectSourceCode/src/init_data/create.sql
@@ -0,0 +1,36 @@
+CREATE TABLE IF NOT EXISTS users (
+  user_id SERIAL PRIMARY KEY NOT NULL,
+  username CHAR(15) NOT NULL,
+  password CHAR(15) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS users_to_classes (
+  user_id SERIAL PRIMARY KEY NOT NULL,
+  class_id INT NOT NULL,
+);
+
+CREATE TABLE IF NOT EXISTS users_to_bookmarks (
+  user_id INT NOT NULL,
+  question_id INT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS users_to_asked_questions (
+  user_id INT NOT NULL,
+  question_id INT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS classes (
+  class_id SERIAL PRIMARY KEY NOT NULL,
+  class_name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS classes_to_questions (
+  class_id SERIAL PRIMARY KEY NOT NULL,
+  question_id FOREIGN KEY NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS questions (
+  question_id SERIAL PRIMARY KEY NOT NULL,
+  question_name VARCHAR(50) NOT NULL,
+  questions_info VARCHAR(150) NOT NULL
+);
