f8aab0fc612536d736800a5263671a0b5f7fed90
Siraaj Sandhu <143656182+siraajsandhu@users.noreply.github.com>
2024-11-21 12:30:15 -0700

    Update index.js

--- ProjectSourceCode/src/index.js
+++ ProjectSourceCode/src/index.js
@@ -19,8 +19,8 @@ const hbs = handlebars.create({
 
 // database configuration
 const dbConfig = {
-  host: 'db', // the database server
-  port: 5432, // the database port
+  host: process.env.POSTGRES_HOST, // the database server
+  port: process.env.POSTGRES_PORT, // the database port
   database: process.env.POSTGRES_DB, // the database name
   user: process.env.POSTGRES_USER, // the user account to connect with
   password: process.env.POSTGRES_PASSWORD, // the password of the user account
