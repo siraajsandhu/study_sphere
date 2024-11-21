a7906b892708c7bd8ec661ffe970903201636066
Henry Van Cleave <heva2420@colorado.edu>
2024-11-21 12:46:25 -0700

    adding init_db.sh

---
+++ ProjectSourceCode/init_db.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# DO NOT PUSH THIS FILE TO GITHUB
+# This file contains sensitive information and should be kept private
+
+# TODO: Set your PostgreSQL URI - Use the External Database URL from the Render dashboard
+PG_URI="postgresql://users_db_29s1_user:vPBr30fndcNAnFtbd5NPgN1rMitmUhxs@dpg-csvobid2ng1s73du6peg-a/users_db_29s1"
+
+# Execute each .sql file in the directory
+for file in init_data/*.sql; do
+    echo "Executing $file..."
+    psql $PG_URI -f "$file"
+done
+cd z
